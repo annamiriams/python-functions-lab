83fc57e66ee25f1285c6389c7c0b3b8ecb01d797
annams <annaextrasteckel@gmail.com>
2025-05-05 19:42:01 -0700

    exercise 5 complete

--- exercises.py
+++ exercises.py
@@ -78,3 +78,20 @@ print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
 
 # ----------------------------------------------------------------------------------------------------------------------------------
 
+# Exercise 5: Sum to N
+#
+# Write a function named `sum_to` that takes a single integer n and returns the sum of all integers from 1 to n.
+#
+# Examples:
+# sum_to(6) should return 21.
+# sum_to(10) should return 55.
+#
+# Define the function and then call it below.
+
+def sum_to(n):
+    return sum(range(1, n + 1))
+    
+print('Exercise 5:', sum_to(6))
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
